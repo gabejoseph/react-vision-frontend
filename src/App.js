bbd634471276b87645df9d33e23507d52c504f1b
gabejoseph <gabriel.joseph4@gmail.com>
2020-08-27 17:50:36 -0400

    working on client / vision.imageannotatorclient

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import './App.css';
 
+
+
 const vision = require('@google-cloud/vision')
 
-const client = new vision.ImageAnnotatorClient({
-  keyFilename: './react-vision-frontend/React-Vision-Frontend-fede680a0b8a.json'
-});
+console.log(vision)
 
-client
-  .labelDetection('./resources/1200px-Florida_Box_Turtle_Digon3_re-edited.jpg')
-  .then(results => {
-    const labels = results[0].labelAnnotations;
-    console.log('Labels:');
-    labels.forEach(label => console.log(label.description))
-  })
-  .catch(err => console.log('ERROR', err));
+
+const client = new vision.ImageAnnotatorClient();
+
+// client
+//   .labelDetection('./resources/1200px-Florida_Box_Turtle_Digon3_re-edited.jpg')
+//   .then(results => {
+//     const labels = results[0].labelAnnotations;
+//     console.log('Labels:');
+//     labels.forEach(label => console.log(label.description))
+//   })
+//   .catch(err => console.log('ERROR', err));
   
 
 function App() {
