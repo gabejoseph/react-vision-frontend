a356d4797e177e0cc97a07306af45ae6b521a7ad
gabejoseph <gabriel.joseph4@gmail.com>
2020-08-27 13:35:58 -0400

    adding in client

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import './App.css';
 
+const vision = require('@google-cloud/vision')
 
+const client = new vision.ImageAnnotatorClient();
+
+client
+  .labelDetection('./resources/1200px-Florida_Box_Turtle_Digon3_re-edited.jpg')
+  .then(results => {
+    const labels = results[0].labelAnnotations;
+    console.log('Labels:');
+    labels.forEach(label => console.log(label.description))
+  })
+  .catch(err => console.log('ERROR', err));
+  
 
 function App() {
   return (
