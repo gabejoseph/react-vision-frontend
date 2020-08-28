33955d543379efc0789d12d7d2557235c7774a5c
gabejoseph <gabriel.joseph4@gmail.com>
2020-08-28 11:47:58 -0400

    clearing files in public

--- src/App.js
+++ src/App.js
@@ -3,23 +3,7 @@ import './App.css';
 
 
 
-// async function quickstart() {
-//   // Imports the Google Cloud client library
-  const vision = require('@google-cloud/vision');
 
-//   // Creates a client
-  const client = new vision.ImageAnnotatorClient({
-    oauth: '7GPg2YnMG8dQNZb2AYHGZ2Yg'
-  });
-
-  console.log(client)
-
-//   // Performs label detection on the image file
-//   const [result] = await client.labelDetection('./resources/wakeupcat.jpg');
-//   const labels = result.labelAnnotations;
-//   console.log('Labels:');
-//   labels.forEach(label => console.log(label.description));
-// }
   
 
 function App() {
@@ -31,3 +15,22 @@ function App() {
 }
 
 export default App;
+
+
+// async function quickstart() {
+// //   // Imports the Google Cloud client library
+//   const vision = require('@google-cloud/vision');
+
+// //   // Creates a client
+//   const client = new vision.ImageAnnotatorClient({
+//     oauth: '7GPg2YnMG8dQNZb2AYHGZ2Yg'
+//   });
+
+//   console.log(client)
+
+//   // Performs label detection on the image file
+//   const [result] = await client.labelDetection('./resources/wakeupcat.jpg');
+//   const labels = result.labelAnnotations;
+//   console.log('Labels:');
+//   labels.forEach(label => console.log(label.description));
+// }
