5e43623dd8de0fda62ba0b64988a9558addf45a4
gabejoseph <gabriel.joseph4@gmail.com>
2020-09-01 14:35:46 -0400

    flex 1

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,11 @@ import './Header.css'
 function Header() {
     return (
         <div className="header" >
-            
+            <img 
+                className="header_icon"
+                src="https://allpointsco.com/apm/wp-content/uploads/2016/09/icon-sight.png"
+                alt=""
+            />
         </div>
     )
 }
