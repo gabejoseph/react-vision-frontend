bffcc422e56dcdd49bfc85e481ee556f2fa46fdb
gabejoseph <gabriel.joseph4@gmail.com>
2020-09-01 15:47:55 -0400

    banner component

---
+++ src/components/Banner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Banner.css'
+
+function Banner() {
+    return (
+        <div className='banner' >
+            <p>Hi from Banner</p>
+        </div>
+    )
+}
+
+export default Banner
