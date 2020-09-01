724873279ac8127f6d2c6621bb4e7c8d53ffc596
gabejoseph <gabriel.joseph4@gmail.com>
2020-09-01 18:23:23 -0400

    adding in styling for buttons

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,6 +5,13 @@ import { Button } from '@material-ui/core'
 function Banner() {
     return (
         <div className='banner' >
+            <div className='banner_search'>
+                <Button
+                    className='banner_searchButton'
+                    variant='outlined'>
+                    Search Dates
+                </Button>
+            </div>
             <div className='banner_info'>
                 <h1>Get out and stretch your imagination</h1>
                 <h5>Plan a different kind of getaway to uncover the hidden gems near you.</h5>
