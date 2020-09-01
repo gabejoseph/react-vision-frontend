9350fb99991339c287c695e2bdc1153ebb70c625
gabejoseph <gabriel.joseph4@gmail.com>
2020-09-01 14:51:22 -0400

    adding in header center

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Header.css'
+import SearchIcon from "@material-ui/icons/Search"
 
 function Header() {
     return (
@@ -9,7 +10,12 @@ function Header() {
                 src="https://allpointsco.com/apm/wp-content/uploads/2016/09/icon-sight.png"
                 alt=""
             />
+            <div className="header_center">
+                <input type="text" />
+                <SearchIcon />
+            </div>
         </div>
+
     )
 }
 
