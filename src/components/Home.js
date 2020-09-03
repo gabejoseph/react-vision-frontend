fb37dc9cea7ed1d430862b0511325da4a6ce1dbd
gabejoseph <gabriel.joseph4@gmail.com>
2020-09-02 20:27:30 -0400

    adding card styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,35 @@
 import React from 'react'
 import './Home.css'
 import Banner from './Banner'
+import Card from './Card'
 
 function Home() {
     return (
         <div className='home'>
             <Banner />
+
+            <div className='home_section'>
+                <Card 
+                    src="https://a0.muscache.com/im/pictures/eb9c7c6a-ee33-414a-b1ba-14e8860d59b3.jpg?im_w720"
+                    title="Online Experiences"
+                    description="Unique activities we can do together, led by a world of hosts."
+                />
+                <Card 
+                    src="https://a0.muscache.com/im/pictures/eb9c7c6a-ee33-414a-b1ba-14e8860d59b3.jpg?im_w720"
+                    title="Online Experiences"
+                    description="Unique activities we can do together, led by a world of hosts."
+                />
+                <Card 
+                    src="https://a0.muscache.com/im/pictures/eb9c7c6a-ee33-414a-b1ba-14e8860d59b3.jpg?im_w720"
+                    title="Online Experiences"
+                    description="Unique activities we can do together, led by a world of hosts."
+                />
+            </div>
+            <div className='home_section'>
+                <Card />
+                <Card />
+                <Card />
+            </div>
         </div>
     )
 }
