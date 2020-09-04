2ef9ec9361ec455136a79b8b2f8cfad68be9822f
gabejoseph <gabriel.joseph4@gmail.com>
2020-09-03 20:18:47 -0400

    adding switches in

--- src/App.js
+++ src/App.js
@@ -4,29 +4,23 @@ import Home from './components/Home'
 import Header from './components/Header'
 import Footer from './components/Footer'
 // import Map from './components/Map'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 
 
 function App() {
   return (
       <div className="app" >
+        <Router >
+          <Header />
+          <Switch>
+            <Home />
+          {/* <Map /> */}
+            <SearchPage />
+          </ Switch>
+          <Footer />
+        </ Router>
 
-      <Header />
-      <Home />
-      {/* <Map /> */}
-      <Footer />
-
-        {/* Home */}
-          {/* Header */}
-
-          {/* Banner */}
-            {/* Search */}
-
-          {/* Cards */}
-
-
-        {/* SearchPage */}
-          {/* ... */}
 
       </div>
   );
