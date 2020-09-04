c129ee61938c6bc3866a3418a0b94a42c22d8087
gabejoseph <gabriel.joseph4@gmail.com>
2020-09-04 10:18:43 -0400

    adding in SearchPage component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Home from './components/Home'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import SearchPage from './components/SearchPage'
 // import Map from './components/Map'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
@@ -12,13 +13,23 @@ function App() {
   return (
       <div className="app" >
         <Router >
+
           <Header />
+
           <Switch>
-            <Home />
-          {/* <Map /> */}
-            <SearchPage />
+            <Route path="/search" >
+              <SearchPage />
+            </Route>
+            <Route path="/" >
+              <Home />
+            </Route>
           </ Switch>
+
+          {/* <Map /> */}
+
+
           <Footer />
+
         </ Router>
 
 
