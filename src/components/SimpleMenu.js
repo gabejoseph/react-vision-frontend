72dd4e63c3ba9dcef5b9d68af6fed5e226844eda
gabejoseph <gabriel.joseph4@gmail.com>
2020-09-10 13:14:42 -0400

    identifying cross origin issue

--- src/components/SimpleMenu.js
+++ src/components/SimpleMenu.js
@@ -4,75 +4,37 @@ import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import { Avatar } from "@material-ui/core"
 import { Link } from 'react-router-dom'
-import { connect } from 'react-redux'
-import userActions from '../actions/user.actions'
 
+export default function SimpleMenu() {
 
-class SimpleMenu extends React.Component {
+  const [anchorEl, setAnchorEl] = React.useState(null);
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      anchorEl: null
-    }
-
-    this.handleClick = this.handleClick.bind(this)
-    this.handleClose = this.handleClose.bind(this)
-    this.handleLogout = this.handleLogout.bind(this)
-  }
-
-  handleClick(event) {
-    this.setState({
-      anchorEl: event.currentTarget
-    })
-  }
-
-  handleClose() {
-    this.setState({
-      anchorEl: null
-    })
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
   };
 
-  handleLogout() {
-    this.props.logout();
-  }
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
-
-  render() {
-    const user = JSON.parse(localStorage.user)
-
-    return (
-      <div>
-          <Button aria-controls="simple-menu" aria-haspopup="true" onClick={this.handleClick}>
-              <Avatar />
-          </Button>
-          <Menu
-              id="simple-menu"
-              anchorEl={this.state.anchorEl}
-              keepMounted
-              open={Boolean(this.state.anchorEl)}
-              onClose={this.handleClose}
-          >
-            {user ? <MenuItem onClick={this.handleLogout}>Logout</MenuItem> : 
-              <>
-                <Link to='/login' ><MenuItem onClick={() => this.handleClose}>Login</MenuItem></Link>
-                <MenuItem onClick={this.handleLogout}>Logout</MenuItem>   
-              </>
-            }
-          </Menu>
-      </div>
-    );
-  }
+  return (
+    <div>
+        <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+            <Avatar />
+        </Button>
+        <Menu
+            id="simple-menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+        >
+            <Link to='/login' >
+                <MenuItem onClick={handleClose}>Login</MenuItem>
+            </Link>
+            <MenuItem onClick={handleClose}>Logout</MenuItem>
+        </Menu>
+    </div>
+  );
 }
 
-const actionCreators = {
-  logout: userActions.logout
-};
-
-export default connect(null, actionCreators)(SimpleMenu)
-
-// function mapState(state) {
-//   const { alert } = state;
-//   return { alert };
-// }
