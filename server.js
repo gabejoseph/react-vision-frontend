ed49992f935555cca816745a3c9cc548cbbcfeea
gabejoseph <gabriel.joseph4@gmail.com>
2020-09-20 17:17:12 -0400

    editing server.js

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
+
 const express = require('express');
-const favicon = require('express-favicon');
 const path = require('path');
-const port = process.env.PORT || 3000;
-const app = express();
-app.use(favicon(__dirname + '/build/favicon.ico'));
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-app.listen(port);
+const PORT = process.env.PORT || 5000;
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/search', (req, res) => res.send(search()))
+  .listen(PORT);
