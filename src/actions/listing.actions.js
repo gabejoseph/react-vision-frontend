ab652b4fac850934f6529037b8139f8d7a5a4378
gabejoseph <gabriel.joseph4@gmail.com>
2020-12-04 11:01:03 -0500

    listing update updatelistings() created

--- src/actions/listing.actions.js
+++ src/actions/listing.actions.js
@@ -4,22 +4,6 @@ import service from '../services/listing.service'
 import { alertActions } from './alert.actions';
 import listingConstants from '../constants/listing.constants';
 
-const getListings = () => {
-    return dispatch => {
-        dispatch(request());
-        service.getAll()
-            .then(
-                listings => { 
-                    dispatch(success(listings));
-                    // history.push('/');
-                },
-            );
-        const request = () => { return { type: constants.GETALL_REQUEST } }
-        const success = (listing) => { return { type: constants.GETALL_SUCCESS, listing } }
-        
-    };
-}
-
 const createListing = (listing) => {
     return dispatch => {
         dispatch(request());
@@ -42,9 +26,48 @@ const createListing = (listing) => {
 
 }
 
+const getListings = () => {
+    return dispatch => {
+        dispatch(request());
+        service.getAll()
+            .then(
+                listings => { 
+                    dispatch(success(listings));
+                    // history.push('/');
+                },
+            );
+        const request = () => { return { type: constants.GETALL_REQUEST } }
+        const success = (listing) => { return { type: constants.GETALL_SUCCESS, listing } }
+        
+    };
+}
+
+const updateListings = (listing) => {
+    return dispatch => {
+        dispatch(request());
+        service.update(listing)
+            .then(
+                listing => {
+                    dispatch(success(listing));
+                    history.push('/search');
+                    dispatch(alertActions.success('Listing updated'))
+                },
+                error => {
+                    dispatch(failure(error.toString()));
+                    dispatch(alertActions.error.toString());
+                }
+            )
+        const request = () => { return { type: constants.GETALL_REQUEST } }
+        const success = (listing) => { return { type: constants.GETALL_SUCCESS, listing } }
+        const failure = (error) => { return { type: listingConstants.LISTING_FAILURE, error } }
+    }
+}
+
+
 export const listingActions = { 
     getListings, 
-    createListing
+    createListing,
+    updateListings
 }
 
 
