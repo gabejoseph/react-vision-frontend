e1348ad9171931db42989ea3230e5e308c1d6b44
gabejoseph <gabriel.joseph4@gmail.com>
2020-12-16 10:20:17 -0500

    updated form and inputs

--- src/components/NewListing.js
+++ src/components/NewListing.js
@@ -7,60 +7,86 @@ import Button from '@material-ui/core/Button'
 import { userActions } from '../actions/user.actions';
 
 class Login extends React.Component {
+
     constructor(props) {
         super(props);
 
         // reset login status
 
         this.state = {
-            email: '',
-            password: '',
-            submitted: false
+            img: '',
+            location: '',
+            title: '',
+            description: '',
+            star: '',
+            price: ''
         };
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleChange(e) {
+    handleChange = (e) => {
         const { name, value } = e.target;
         this.setState({ [name]: value });
+        debugger
     }
 
-    handleSubmit(e) {
+    handleSubmit = (e) => {
         e.preventDefault();
 
         this.setState({ submitted: true });
-        const { email, password } = this.state;
-        if (email && password) {
-            this.props.login(email, password);
+        const { img, location, title, description, star, price } = this.state;
+        const user_id = JSON.parse(localStorage.user).user.user_id
+        if (user_id && img && location && title && description && star && price) {
+            // this.props.login(email, password);
             this.props.history.push('/');
         }
     }
 
     render() {
         const { loggingIn } = this.props;
-        const { email, password, submitted } = this.state;
+        const { img, location, title, description, star, price, submitted } = this.state;
         return (
             <div>
-                <h2>Login</h2>
+                <h2>New Listing</h2>
                 <form name="form" onSubmit={this.handleSubmit}>
-                    <div className={'form-group' + (submitted && !email ? ' has-error' : '')}>
-                        <TextField type="text" label="Email" htmlFor="email" className="form-control" name="email" value={email} onChange={this.handleChange} />
-                        {submitted && !email &&
-                            <div className="help-block">Email is required</div>
+                    <div className={'form-group' + (submitted && !img ? ' has-error' : '')}>
+                        <TextField type="text" label="Img" htmlFor="img" className="form-control" name="img" value={img} onChange={this.handleChange} />
+                        {submitted && !img &&
+                            <div className="help-block">An Image URL is required</div>
                         }
                     </div>
-                    <div className={'form-group' + (submitted && !password ? ' has-error' : '')}>
-                        <TextField type="password" htmlFor="password" label="Password" className="form-control" name="password" value={password} onChange={this.handleChange} />
-                        {submitted && !password &&
-                            <div className="help-block">Password is required</div>
+                    <div className={'form-group' + (submitted && !location ? ' has-error' : '')}>
+                        <TextField type="text" htmlFor="location" label="Location" className="form-control" name="location" value={location} onChange={this.handleChange} />
+                        {submitted && !location &&
+                            <div className="help-block">Location is required</div>
+                        }
+                    </div>
+                    <div className={'form-group' + (submitted && !title ? ' has-error' : '')}>
+                        <TextField type="text" htmlFor="title" label="Title" className="form-control" name="title" value={title} onChange={this.handleChange} />
+                        {submitted && !title &&
+                            <div className="help-block">Title is required</div>
+                        }
+                    </div>
+                    <div className={'form-group' + (submitted && !description ? ' has-error' : '')}>
+                        <TextField type="text" htmlFor="description" label="Description" className="form-control" name="description" value={description} onChange={this.handleChange} />
+                        {submitted && !description &&
+                            <div className="help-block">Description is required</div>
+                        }
+                    </div>
+                    <div className={'form-group' + (submitted && !star ? ' has-error' : '')}>
+                        <TextField type="text" htmlFor="star" label="Star" className="form-control" name="star" value={star} onChange={this.handleChange} />
+                        {submitted && !star &&
+                            <div className="help-block">Star is required</div>
+                        }
+                    </div>
+                    <div className={'form-group' + (submitted && !price ? ' has-error' : '')}>
+                        <TextField type="text" htmlFor="price" label="Price" className="form-control" name="price" value={price} onChange={this.handleChange} />
+                        {submitted && !price &&
+                            <div className="help-block">Price is required</div>
                         }
                     </div>
                     <div className="form-group">
-                        <button className="btn btn-primary">Login</button>
-                        {loggingIn}
-                        <Button><Link to="/register" className="btn btn-link">Register</Link></Button>
+                        <Button className="btn btn-primary" type="submit" >Create Listing</Button>
                     </div>
                 </form>
             </div>
@@ -69,13 +95,13 @@ class Login extends React.Component {
 }
 
 function mapState(state) {
-    const { loggingIn } = state.authentication;
-    return { loggingIn };
+    // const { loggingIn } = state.authentication;
+    // return { loggingIn };
 }
 
 const actionCreators = {
-    login: userActions.login,
-    logout: userActions.logout
+    // login: userActions.login,
+    // logout: userActions.logout
 };
 
 export default connect(mapState, actionCreators)(Login);
